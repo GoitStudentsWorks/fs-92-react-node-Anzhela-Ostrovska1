b3e2b0277380171febb7cddfe5be90f27b5b3955
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-04-17 22:00:39 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/fs-92-react-node-Anzhela-Ostrovska1@32eac8707cca8fc6e50b3683f8767e01e6f28965 🚀

---
+++ assets/bg_element_des@1x-82ed4fe0.js
@@ -0,0 +1,1 @@
+const e="/fs-92-react-node-Anzhela-Ostrovska1/assets/bg_element_des@1x-86a3ec5e.png";export{e as b};
---
+++ assets/bg_element_des@2x-af4760d3.js
@@ -0,0 +1,1 @@
+const e="/fs-92-react-node-Anzhela-Ostrovska1/assets/bg_element_des@2x-ed159c5b.png";export{e as b};
